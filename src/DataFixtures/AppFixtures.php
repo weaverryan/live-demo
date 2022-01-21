e79edf83575ac7ed1c4b8ec9ce9189fcd5f0faa5
Ryan Weaver <ryan@thatsquality.com>
2022-01-21 11:03:15 -0500

    updating DoctrineFixturesBundle recipe

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -8,7 +8,7 @@ use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager)
+    public function load(ObjectManager $manager): void
     {
         PostFactory::createOne([
             'content' => 'This content uses **markdown** inside!'
